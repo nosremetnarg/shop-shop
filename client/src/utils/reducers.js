96a6aac1ecdfd13217f8b0a067bb9efa0087199e
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-26 12:38:12 -0500

    globalize state

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,40 @@
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from "./actions";
+
+import { useReducer } from 'react';
+
+
+export const reducer = (state, action) => {
+    // import { useReducer } from 'react';  or is this import needed here????
+
+
+    switch (action.type) {
+        // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products]
+            };
+        // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories]
+            };
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            };
+
+        default:
+            return state;
+    }
+};
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
