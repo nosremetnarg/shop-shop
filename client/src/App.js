d8ff0764f9aa4f5fdf91ab2576972e5be4e104c4
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-28 16:37:06 -0500

    shop shop complete

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Success from './pages/Success';
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 
@@ -38,6 +39,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
